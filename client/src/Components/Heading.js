74fe2b9d900ffc9321760f236901794af1f3d7df
신준혁 <oridori2705@naver.com>
2023-06-28 19:39:26 +0900

    네비게이션 바 로그인 로그아웃 기능 추가

--- client/src/Components/Heading.js
+++ client/src/Components/Heading.js
@@ -4,23 +4,39 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { useSelector } from 'react-redux';
+import firebase from '../firebase.js';
+import { useNavigate } from 'react-router-dom';
 
 export const Heading = () => {
+  const user =useSelector((state)=>state.user);
+  const navigate = useNavigate();
+
+  const LogoutHandler = () =>{
+    firebase.auth().signOut();
+    navigate("/")
+  }
   return (
     <div>
       <Navbar bg="dark" expand="lg" variant="dark">
       <Container>
-        <Navbar.Brand href="#home">Community</Navbar.Brand>
+        <Navbar.Brand href="/">Community</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
                   <Nav.Link href="/">Home</Nav.Link>
                   <Nav.Link href="/Upload">Upload</Nav.Link>
-                  <Nav.Link href="/login">Login</Nav.Link>
+                  
 
             
           </Nav>
         </Navbar.Collapse>
+        <Navbar.Brand className='justify-content-end'>
+          {user.accessToken ? (<Navbar.Text onClick={()=>LogoutHandler()}>Logout</Navbar.Text>
+            ): (<Nav.Link href="/login">Login</Nav.Link>)}
+          
+        </Navbar.Brand>
+        
       </Container>
     </Navbar>
 
